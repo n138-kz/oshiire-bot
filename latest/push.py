f84540f5149fa213965e441fec132ef347d6cf03
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-03-29 13:17:44 +0900

    Add rev info #7

--- latest/push.py
+++ latest/push.py
@@ -5,6 +5,7 @@ import datetime
 import os
 import sys
 import yaml
+import math
 
 # refs docs: https://birdie0.github.io/discord-webhooks-guide/discord_webhook.html
 # refs docs: https://qiita.com/ABBBB/items/e6bdf7fc94b8f6f72a01
@@ -90,6 +91,9 @@ payload2 = {
                 "timestamp"     : datetime.datetime.now().isoformat(),
                 "image": {
                     "url"       : "attachment://images_001.png"
+                },
+                "footer": {
+                    "text"      : "Rev: "+str( math.floor( os.path.getmtime( __file__ ) ) ),
                 },
                 "fields": [
                     {
