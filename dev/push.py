b9d9120976415964a5da260b32bf1ab6253a9148
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-03-30 12:01:35 +0900

    Update push.py

--- dev/push.py
+++ dev/push.py
@@ -94,7 +94,7 @@ def main():
                         "url"       : "attachment://images_001.png"
                     },
                     "footer": {
-                        "text"      : "Rev: " + str( math.floor( os.path.getmtime( __file__ ) ) ) + "\n"
+                        "text"      : "Rev: " + str( math.floor( os.path.getmtime( __file__ ) ) ) + ""
                                         + "/" + str( datetime.datetime.fromtimestamp( math.floor( os.path.getmtime( __file__ ) ), datetime.timezone(datetime.timedelta(hours=9)) ) ) + "\n",
                     },
                     "fields": [
