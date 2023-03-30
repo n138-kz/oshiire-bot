c26082e8c8de9762f2d953130ac18e8ab2a4a416
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-03-30 16:14:02 +0900

    Update push.py

--- dev/push.py
+++ dev/push.py
@@ -174,7 +174,7 @@ def main():
 
 
     payload2['payload_json'] = json.dumps( payload2['payload_json'], ensure_ascii=False )
-    curl_res = requests.post(api_url, files = files_images  , data = payload2 )
+    curl_res = requests.post(api_url, files = files_images, data = payload2 )
     #print(curl_res.status_code) # HTTPのステータスコード取得
     #print(curl_res.text)        # レスポンスのHTMLを文字列で取得
 
