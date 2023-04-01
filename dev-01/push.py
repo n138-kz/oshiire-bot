62092d9fb90e40c9eef738d66b9efe669f681dde
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-04-01 09:52:24 +0900

    Update push.py

--- dev-01/push.py
+++ dev-01/push.py
@@ -48,8 +48,8 @@ def main():
                     "description": "複数枚画像投稿テスト",
                     "author": {
                         "name": "みんなのまま",
-                        "url": "https://twitter.com/GrapeColorSoft",
-                        "icon_url": "https://pbs.twimg.com/profile_images/1063236006135062528/493Dm2lD_bigger.jpg",
+                        "url": "https://github.com/n138-kz/oshiire-bot",
+                        "icon_url": "https://upload-os-bbs.hoyolab.com/upload/2022/11/24/21d71c44827fa664a6a21977d9a3dd1c_6799685155050732642.png?x-oss-process=image/resize,s_1000/quality,q_80/auto-orient,0/interlace,1/format,png",
                         "fields": [],
                     },
                     "url": "https://twitter.com/GrapeColorSoft/status/1205289368786620416",
