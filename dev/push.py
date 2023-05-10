28ce35a105995f0b739912948a345194d9071026
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-05-10 18:04:29 +0900

    Update push.py

--- dev/push.py
+++ dev/push.py
@@ -148,7 +148,7 @@ def main():
             'name'  : "原神 (Genshin Impact)",
             'value' : ''
                     + ''
-                    + "- [HoYoLAB: Daily Bounus](https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&lang=ja-jp)\n"
+                    + '- [HoYoLAB: Daily Bounus](https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481&lang=ja-jp)\n'
                     + '',
         },
     )
