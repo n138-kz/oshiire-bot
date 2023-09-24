eebda28023f0b4a2ccbe8b44340869f5aed88ebb
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2023-09-25 01:58:26 +0900

    七聖召喚 Ver.4.1アップデート詳細

--- prd/push.py
+++ prd/push.py
@@ -181,6 +181,7 @@ def main():
 					+ '- [Ver.4.0新聖遺物紹介](https://www.hoyolab.com/article/20789174)\n'
 					+ '- [Ver.4.1公式PV](https://www.youtube.com/watch?v=LsZx11uXVEw)\n'
 					+ '- [Ver.4.1特設ページ](https://hoyo.link/azPeFHAL)\n'
+					+ '- [Ver.4.1「七聖召喚」アップデート詳細](https://genshin.hoyoverse.com/ja/news/detail/113079)\n'
 					+ '',
 		},
 	)
