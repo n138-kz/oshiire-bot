c0f267d4eb0be8d4c1d033fcc06692d6e2368d0f
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-06-30 05:28:07 +0900

    4.5 ... 4.7

--- prd/push.py
+++ prd/push.py
@@ -184,17 +184,42 @@ def main():
 			'name'  : "",
 			'value' : ''
 					+ ''
+					+ '## Ver 4.7'
+					+ '\n'
+					+ ''
+					+ '- [Ver.4.7公式PV](https://youtu.be/PLwJHG1Kt1g)\n'
+					+ '- [Ver.4.7アプデ情報まとめ](https://hoyo.link/8DKjFBAL)\n'
+					+ '- [壁紙:キービジュアル](https://hoyo.link/6d6jFBAL)\n'
+					+ '',
+		},
+	)
+	payload2['payload_json']['embeds'][0]['fields'].append(
+		{
+			'inline': False,
+			'name'  : "",
+			'value' : ''
+					+ ''
+					+ '## Ver 4.6'
+					+ '\n'
+					+ ''
+					+ '- [Ver.4.6公式PV](https://youtu.be/iDC2Rwx8WxY)\n'
+					+ '- [Ver.4.6アプデ情報まとめ](https://hoyo.link/aQ4iFBAL)\n'
+					+ '- [Ver.4.6スタンプ配布](https://hoyo.link/bSmiFBAL)\n'
+					+ '- [壁紙:そよ風とドリンクの旅](https://hoyo.link/9y8iFBAL)\n'
+					+ '',
+		},
+	)
+	payload2['payload_json']['embeds'][0]['fields'].append(
+		{
+			'inline': False,
+			'name'  : "",
+			'value' : ''
+					+ ''
 					+ '## Ver 4.5'
 					+ '\n'
 					+ ''
 					+ '- [Ver.4.5公式PV](https://youtu.be/Z6ZKPOBbwkQ)\n'
 					+ '- [Ver.4.5アプデ情報まとめ](https://hoyo.link/0bSCBYAd)\n'
-					+ ''
-					+ '### Ver 4.4 コラボイベント'
-					+ '\n'
-					+ ''
-					+ '- [原神×スシロー コラボイベント@2/21-3/24](https://www.akindo-sushiro.co.jp/genshin/)\n'
-					+ '- [原神公式サイトのコード入力ページ](https://genshin.hoyoverse.com/ja/gift)\n'
 					+ '',
 		},
 	)
